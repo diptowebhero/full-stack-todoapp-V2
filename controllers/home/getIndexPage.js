fe5c62e4c6406c645415c5c326c9f8c8ed22ebfa
diptowebhero <diptodas1672@gmail.com>
2022-09-07 21:05:22 +0600

    done

--- controllers/home/getIndexPage.js
+++ controllers/home/getIndexPage.js
@@ -2,6 +2,7 @@ const Todo = require("../../models/Todo");
 const User = require("../../models/User");
 
 const getIndexPage = async (req, res, next) => {
+  // console.log("hello");
   try {
     const todos = await Todo.find({ user: req.email });
     const user = await User.findOne({ user: req.email });
